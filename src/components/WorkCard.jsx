87ee61f5d10c4d5b096dd9a869a35c30d686fe7a
unknown <mdalifhasanredoy@gmail.com>
2025-12-09 13:35:13 +0600

    add work part

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -5,7 +5,7 @@ import SubHeading from '../components/SubHeading'
 
 const WorkCard = ({image,title,text}) => {
     return (
-        <div className='w-[424px] rounded-[20px] hover:shadow-lg duration-300'>
+        <div className='w-[424px] rounded-[20px] hover:shadow-lg duration-300 bg-white'>
             <Image src={image} />
             <div className='px-[42px]'>
                 <h3 className='text-2xl text-center text-third font-bold font-inter pt-[30px] pb-[10px]'>{title}</h3>
